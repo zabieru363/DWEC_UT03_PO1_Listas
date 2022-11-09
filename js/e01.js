34ed834869348339ddaaff6f0234178a52b8dee2
zabieru363 <zabierujlc@gmail.com>
2022-11-09 16:34:28 +0100

    Objetos book creados en la función de testeo

--- js/e01.js
+++ js/e01.js
@@ -1,7 +1,49 @@
 "use strict";
 
 function test() {
+    const book1 = {
+      ISBN: "978-84-9804-654-0",
+      title: "El Quijote",
+      author: "Miguel de Cervantes",
+      publicationDate: new Date(1605, 0, 1),
+      price: 20,
+    };
 
+    const book2 = {
+      ISBN: "843-07-8680-814-9",
+      title: "El arte de la guerra",
+      author: "Nora Steinbrun",
+      publicationDate: new Date(2018, 10, 30),
+      price: 32,
+    };
+
+    const book3 = {
+      ISBN: "914-46-6086-292-7",
+      title: "El libro negro del programador",
+      author: "Rafael Gómez Blanes",
+      publicationDate: new Date(2014, 3, 4),
+      price: 20.45,
+    };
+
+    const book4 = {
+      ISBN: "411-06-3922-591-3",
+      title: "Un nuevo thriller trepidante",
+      author: "Karen M. McManus",
+      publicationDate: new Date(2018, 5, 22),
+      price: 18,
+    };
+
+    // Objeto vácio para hacer las pruebas.
+    const book5 = {};
+
+    // Objeto con un ISBN inválido para hacer pruebas.
+    const book6 = {
+        ISBN: "41591-3",
+        title: "Libro random",
+        author: "Jugador de LoL promedio",
+        publicationDate: new Date(2021, 11, 3),
+        price: 18,
+      };
 }
 
 test();
